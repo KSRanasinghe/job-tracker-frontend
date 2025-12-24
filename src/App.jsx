07f7d80cfb3e1c8a3b92f4f305a94006ac6bc652
KSRanasinghe <kesharashakthi71@gmail.com>
2025-12-24 22:29:26 +0530

    fix: handle root route redirect to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import AddJob from './pages/AddJob';
@@ -10,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Navigate to="/dashboard"/>}/>
         <Route path='/login' element={<Login />} />
 
         <Route path='/dashboard'
