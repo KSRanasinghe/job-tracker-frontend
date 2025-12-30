0b6ddf8678d41fb802f5fc4922044bcb16c14f7d
KSRanasinghe <kesharashakthi71@gmail.com>
2025-12-30 12:12:14 +0530

    add user profile dropdown with dynamic user info

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,49 +1,66 @@
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import AuthBtn from '../components/AuthBtn';
+import './MainLayout.css'
 
 function MainLayout({ children }) {
-  const { token, logout } = useAuth();
+  const { logout, user } = useAuth();
+  const initial = user?.firstName?.charAt(0).toUpperCase();
 
   return (
-    <div>
-      <nav style={{ padding: '1rem', background: '#222' }}>
-        <Link to="/dashboard" style={{ color: '#fff', marginRight: '1rem' }}>
-          Dashboard
-        </Link>
+    <>
+      <div className="container-fluid main-navbar-section global">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-2 content-1">
+              <Link to="/">
+                <img src="images/logo-nav.png" className="img-fluid" alt="ApplyHub" />
+              </Link>
+            </div>
+            <div className="col-12 col-lg-10 content-2">
+              <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                  <a className="navbar-brand d-none" href="#">Navbar</a>
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav">
+                      <div class="dropdown">
+                        <a class="profile-btn" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          {initial}
+                        </a>
+                        <ul class="dropdown-menu">
+                          <div className='profile'>
+                            <p className='profile-icon'>{initial}</p>
+                            <h5>{user.firstName} {user.lastName}</h5>
+                            <h6>{user.email}</h6>
+                          </div>
+                          <li><hr class="dropdown-divider" /></li>
+                          <li>
+                            <button
+                              onClick={logout}
+                              className='dropdown-item'
+                            >
+                              Log out
+                            </button>
+                          </li>
+                        </ul>
+                      </div>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
 
-        <Link to="/add-job" style={{ color: '#fff', marginRight: '1rem' }}>
-          Add Job
-        </Link>
-
-        {!token ? (
-          <>
-            <Link to="/login" style={{ color: '#fff', marginRight: '1rem' }}>
-              Sign In
-            </Link>
-
-            <Link to="/register" style={{ color: '#fff' }}>
-              Sign Up
-            </Link>
-          </>
-        ) : (
-          <button
-            onClick={logout}
-            style={{
-              background: 'transparent',
-              color: '#fff',
-              border: '1px solid #fff',
-              cursor: 'pointer',
-            }}
-          >
-            Logout
-          </button>
-        )}
-      </nav>
-
-      <main style={{ padding: '1rem' }}>
+      <main>
         {children}
       </main>
-    </div>
+
+    </>
   );
 }
 
